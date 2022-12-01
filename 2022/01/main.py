0125e30d828944338a7841fe124eb24e85464c08
mmyumu <mmyumu@gmail.com>
2022-12-01 14:15:43 +0100

    2022/01: part two

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -5,7 +5,7 @@ def run():
     with open('2022/01/input.txt', 'r') as f:
         lines = f.read().splitlines()
 
-    max_calories = 0
+    calories = []
     current_calories = 0
     elf_number = 1
     for line in lines:
@@ -17,14 +17,20 @@ def run():
             except ValueError:
                 logger.exception("Cannot get calories from line")
         else:
-            if current_calories > max_calories:
-                logger.info(f"Elf number {elf_number} is carrying {current_calories} which is more than the previous maximum ({max_calories}) ")
-                max_calories = current_calories
+            if len(calories) == 0:
+                max_calory = 0
+            else:
+                max_calory = calories[0]
+
+            logger.info(f"Elf number {elf_number} is carrying {current_calories} which is more than the previous maximum ({max_calory}) ")
+            calories.append(current_calories)
+            calories = sorted(calories, reverse=True)
                 
             current_calories = 0
             elf_number += 1
 
-    logger.info(f"Elf number {elf_number} is carrying {max_calories} which is the maximum")
+    logger.info(f"Part one: Elf number {elf_number} is carrying {calories[0]} which is the maximum")
+    logger.info(f"Part two: Sum of calories carried by top three elves: {sum(calories[:3])}")
             
 
 if __name__ == "__main__":
