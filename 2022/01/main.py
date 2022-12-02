5d2b2da8b4a4dac2a3174a4a80ed122c267f34eb
mmyumu <mmyumu@gmail.com>
2022-12-02 11:16:07 +0100

    Improve code quality

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -2,14 +2,16 @@ from logger import logger
 
 
 def run():
-    with open('2022/01/input.txt', 'r') as f:
+    """
+    Entrypoint
+    """
+    with open('2022/01/input.txt', 'r', encoding="utf-8") as f:
         lines = f.read().splitlines()
 
     calories = []
     current_calories = 0
     elf_number = 1
     for line in lines:
-        
         if line:
             try:
                 calory = int(line)
@@ -20,13 +22,13 @@ def run():
             logger.info(f"Elf number {elf_number} is carrying {current_calories}")
             calories.append(current_calories)
             calories = sorted(calories, reverse=True)
-                
+
             current_calories = 0
             elf_number += 1
 
     logger.info(f"Part one: Elf number {elf_number} is carrying {calories[0]} which is the maximum")
     logger.info(f"Part two: Sum of calories carried by top three elves: {sum(calories[:3])}")
-            
+
 
 if __name__ == "__main__":
     run()
